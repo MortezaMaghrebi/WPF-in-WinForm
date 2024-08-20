7187e7818c5503747656d62b46ff6003d6344893
Morteza Maghrebi <M.Maghrebi@argtelecom.com>
2024-08-20 13:22:35 +0330

    init--add project

---
+++ WpfInForm/WpfInForm/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WpfInForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.userControl11 = new WpfInForm.UserControl1();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(91, 50);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(539, 262);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.userControl11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(359, 97);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.elementHost1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private UserControl1 userControl11;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WpfInForm/WpfInForm/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using wpfuc = WpfInForm.UserControl1;
+
+namespace WpfInForm
+{
+    public partial class Form1 : Form
+    {
+        wpfuc uc;
+        public Form1()
+        {
+            InitializeComponent();
+            uc = elementHost1.Child as wpfuc;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            uc.wpftree.Items.Add("Hello");
+        }
+    }
+}
